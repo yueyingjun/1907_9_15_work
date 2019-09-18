c2aa5176de26fc3fb0603233867119d077a6dddf
lijiani <lijiani.com>
2019-09-19 01:00:46 +0800

    work

--- xiechengwork/C-4xuanxinagka.js
+++ xiechengwork/C-4xuanxinagka.js
@@ -15,3 +15,49 @@ function C4xuanxiangka(selector1,selector2) {
         }
     }
 }
+
+function daohang(selector1,selector2) {
+    var links=document.querySelectorAll(selector1)
+    var list=document.querySelectorAll(selector2)
+    for(let i=0;i<links.length;i++){
+        links[i].onmouseover=function () {
+            list[i].style.display="block"
+            links[i].style.background="#fff"
+        }
+        links[i].onmouseout = function () {
+            links[i].style.color=""
+            links[i].style.background=""
+            list[i].style.display = "none"
+        }
+    }
+}
+function daohang11(selector1,selector2) {
+    var links=document.querySelectorAll(selector1)
+    var list=document.querySelectorAll(selector2)
+    for(let i=0;i<links.length;i++){
+        links[i].onmouseover=function () {
+            list[i].style.display="block"
+
+        }
+        links[i].onmouseout = function () {
+            links[i].style.background=""
+            list[i].style.display = "none"
+        }
+    }
+}
+
+function daohang1(selector1,selector2) {
+    var links=document.querySelectorAll(selector1)
+    var list=document.querySelectorAll(selector2)
+    for(let i=0;i<links.length;i++){
+        links[i].onmouseover=function () {
+            list[i].style.display="block"
+            links[i].style.background="#3983e5"
+
+        }
+        links[i].onmouseout = function () {
+            links[i].style.background=""
+            list[i].style.display = "none"
+        }
+    }
+}
